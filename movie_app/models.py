712fb9c6e57275a21a89dee4ff1dd60003548246
Kurz <kurz956kk@gmail.com>
2023-12-16 21:59:51 +0500

    a few data changes

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
-    year = models.IntegerField()
-    budget = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=10000000)
 
     def __str__(self):
         return f'{self.name}  - {self.rating}%'
+
+
+# from movie_app.models import Movie
