db6ade165bb4541fe89b783e4c38d46701beefbe
Kurz <kurz956kk@gmail.com>
2023-12-18 12:14:50 +0500

    slug trying

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from . models import Movie
 # Create your views here.
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    for movie in movies:
+        movie.save()
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
+
+def show_one_movie(request, id_movie:int):
+    #movie = Movie.objects.get(id=id_movie)          вариант через ГЕТ, сломается если дать несуществующий ключ
+    movie = get_object_or_404(Movie, id=id_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie
+    })
--- movie_proj/settings.py
+++ movie_proj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'movie_app',
     'django_extensions',
 
+
 ]
 
 MIDDLEWARE = [
