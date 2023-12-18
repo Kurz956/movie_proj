d6a38ab17cd796aa136bfb4c6b8d45f717a860c9
Kurz <kurz956kk@gmail.com>
2023-12-18 13:39:30 +0500

    slug complete

--- movie_app/models.py
+++ movie_app/models.py
@@ -7,16 +7,15 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=10000000)
-    slug = models.SlugField(default='', null=False)
+    slug = models.SlugField(default='', null=False, db_index=True)
 
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name, allow_unicode=True)
+        self.slug = slugify(self.name)
         super(Movie, self).save(*args, **kwargs)
 
     def get_url(self):
-        return reverse('movie-detail', args=(self.id,))
+        return reverse('movie-detail', args=(self.slug,))
     def __str__(self):
         return f'{self.name}  - {self.rating}%'
 
--- movie_app/views.py
+++ movie_app/views.py
@@ -9,9 +9,9 @@ def show_all_movie(request):
         'movies': movies
     })
 
-def show_one_movie(request, id_movie:int):
+def show_one_movie(request, slug_movie:str):
     #movie = Movie.objects.get(id=id_movie)          вариант через ГЕТ, сломается если дать несуществующий ключ
-    movie = get_object_or_404(Movie, id=id_movie)
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
     })
